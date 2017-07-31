94ed75c08e9720781eb7ac201788037732ce7354
Steve Zaske <github@zaske.com>
2017-07-30 23:50:08 -0700

    Add a clean gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
     return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
@@ -21,6 +24,18 @@ gulp.task("minifyScripts", ["jsBrowserify"], function() {
     return gulp.src("./build/js/app.js")
         .pipe(uglify())
         .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function() {
+    return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function() {
+    if (buildProduction) {
+        gulp.start('minifyScripts');
+    } else {
+        gulp.start('jsBrowserify');
+    }
 });
 
 // gulp.task('jsBrowserify', function() {
